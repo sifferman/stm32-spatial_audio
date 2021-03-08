75afbc2d629a327749d88f516fcf4fcc79f46a2c
E4tHam <43790149+E4tHam@users.noreply.github.com>
2021-03-08 13:21:06 -0800

    fixed c++ code

--- pc/src/comstatus.cpp
+++ pc/src/comstatus.cpp
@@ -3,12 +3,14 @@
 
 #include "comstatus.h"
 
+#include <chrono>
 #include <cstring>
 
 
 ComStatus::ComStatus( SerialPort * const port )
-: port(port), running(1), exit(0), ready(0) {
+: port(port), running(1), exit(0), ready(1) {
     poll_thread = std::thread{&ComStatus::pollRX, this};
+    std::cout << "polling read" << std::endl;
 }
 
 ComStatus::~ComStatus() {
@@ -19,29 +21,33 @@ ComStatus::~ComStatus() {
 void ComStatus::pollRX() {
     while ( !exit ) {
 
+        std::chrono::milliseconds timespan(1);
+        std::this_thread::sleep_for( timespan );
+
         memset( RECIEVE, 0, MAX_DATA_LENGTH + 1 );
         int readErr = port->readSerialPort( RECIEVE, MAX_DATA_LENGTH );
         if ( readErr == 0 ) {
-            exit = 1;
-            break;
+            continue;
         }
 
         size_t size = std::strlen( RECIEVE );
-        if ( size ) {
-            switch ( RECIEVE[ size-1 ] ) {
-                case 'g':
-                    ready = 1;
-                    break;
-                case 'B':
-                    ready = 0;
-                    break;
-                default:
-                    exit = 1;
-                    break;
-            }
+        printf("recieved: \"%s\"\n", RECIEVE);
+        switch ( RECIEVE[ size-1 ] ) {
+            case 'g':
+                if ( ready == 0 ) printf("Enabling.\n");
+                ready = 1;
+                break;
+            case 'B':
+                if ( ready == 1 ) printf("Disabling.\n");
+                ready = 0;
+                break;
+            default:
+                std::cerr << "[WARNING]: Poll recieved bad char \'" << RECIEVE[ size-1 ] << '\'' << std::endl;
+                break;
         }
 
     }
     running = 0;
     ready = 0;
+    std::cout << "Exited polling." << std::endl;
 }
--- pc/src/main.cpp
+++ pc/src/main.cpp
@@ -9,7 +9,7 @@
 #include <fcntl.h>
 #include <io.h>
 
-#define _PACKET_SIZE 4
+#define _PACKET_SIZE ( 1UL << 4 )
 
 const char portName[] = "COM6";
 
@@ -25,15 +25,16 @@ int main( int argc, char * argv[] ) {
         return 1;
     }
 
+    /* Init audio */
     audio = new Wave( argv[1] );
-
     if (   audio->GetNumChannels()  != 2
         || audio->GetBitsPerSample()!= 16
         || audio->GetSampleRate()   != 44100 ) {
         std::cerr << "[ERROR]: Bad wave file format." << std::endl;
-        return 1;
+        return 2;
     }
 
+    /* Init STM32 */
     stm32 = new SerialPort( portName );
     while ( !stm32->isConnected() ) {
         Sleep(100);
@@ -41,26 +42,39 @@ int main( int argc, char * argv[] ) {
         delete stm32;
         stm32 = new SerialPort(portName);
     }
+    std::cout << "Connected!\n";
 
+    /* Init Com Status */
     stm32_status = new ComStatus( stm32 );
 
+    /* Start sending audio samples */
     size_t i = 0;
-    const char * mem = (const char *) audio->getSampleMemory();
-    uint8_t packet_size = audio->getSampleMemorySize() / _PACKET_SIZE;
-    while ( 1 ) {
-        if ( !stm32_status->active() ) break;
-        
+    const uint8_t * mem = (const uint8_t *) audio->getSampleMemory();
+    uint32_t mem_size = audio->getSampleMemorySize();
+
+    while ( stm32_status->active() && stm32->isConnected() ) {
+
+        // std::string s;
+        // std::cout << "Give input:\n";
+        // getline( std::cin, s );
+
         if ( stm32_status->ComIsReady() ) {
-            int writeErr = stm32->writeSerialPort( mem + i, _PACKET_SIZE );
+            // std::cout << "Sending: ";
+            // for ( uint8_t i_print = 0; i_print < _PACKET_SIZE; i_print++ )
+            //     std::cout << (const uint16_t)*(mem + i + i_print) << " ";
+            // std::cout << std::endl;
+
+            int writeErr = stm32->writeSerialPort( (const char*)mem + i, _PACKET_SIZE );
             if ( writeErr == 0 ) break;
-            i = ( i + 1 ) % packet_size;
+            i += _PACKET_SIZE;
+            if ( i >= mem_size ) i = 0;
         }
     }
 
+    std::cout << "Exiting..." << std::endl;
     delete audio;
     delete stm32_status;
     delete stm32;
-
 
     std::cout << "Done." << std::endl;
 
